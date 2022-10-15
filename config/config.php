8622e51a3cec9990e357fcee34197c2bd70fd10a
JohnMarklCabitac <jmcabitac@gmail.com>
2022-10-15 10:58:23 +0800

    first-commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/recordsapp/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'Jm202080461');
+	define('DB_NAME', 'recordsapp'); 
